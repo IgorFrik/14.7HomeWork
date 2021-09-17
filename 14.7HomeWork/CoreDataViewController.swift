06378d97d0813fbabae71efaf45921cf21baf1ef
Igor <murlocov@icloud.com>
2021-09-17 22:59:19 +0700

    Second

---
+++ 14.7HomeWork/CoreDataViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CoreDataViewController.swift
+//  14.7HomeWork
+//
+//  Created by Igor Frik on 17.09.2021.
+//
+
+import UIKit
+
+class CoreDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ 14.7HomeWork/RealmViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RealmViewController.swift
+//  14.7HomeWork
+//
+//  Created by Igor Frik on 17.09.2021.
+//
+
+import UIKit
+
+class RealmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
