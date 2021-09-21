a41a3633cab6e1874e1c65c3ba402adb164cfa5e
Igor <murlocov@icloud.com>
2021-09-21 16:25:29 +0700

    codeData save ready

--- 14.7HomeWork/CoreDataViewController.swift
+++ 14.7HomeWork/CoreDataViewController.swift
@@ -6,44 +6,87 @@
 //
 
 import UIKit
+import CoreData
 
 class CoreDataViewController: UIViewController {
     
     @IBOutlet weak var toDoTable: UITableView!
     @IBOutlet weak var dButton: UIButton!
-    var todoList: [String] = []
     var currentCell: Int = 0
+    var todoList: [NSManagedObject] = []
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        toDoTable.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+      let managedContext = appDelegate.persistentContainer.viewContext
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ToDoCoreData")
+      do {
+        todoList = try managedContext.fetch(fetchRequest)
+      } catch let error as NSError {
+        print("Could not fetch. \(error), \(error.userInfo)")
+      }
+//        self.tableView(toDoTable, didSelectRowAt: toDoTable.indexPathForSelectedRow!)
+    }
+    
+    func save(toDo: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "ToDoCoreData", in: managedContext)!
+        let element = NSManagedObject(entity: entity, insertInto: managedContext)
+        element.setValue(toDo, forKey: "toDo")
+        do {
+            try managedContext.save()
+            todoList.append(element)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+            
+        }
+    }
+    
+    func deleteElement(toDo: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "ToDoCoreData", in: managedContext)!
+        let element = NSManagedObject(entity: entity, insertInto: managedContext)
+        element.setValue(toDo, forKey: "toDo")
+        managedContext.delete(element)
+        todoList.remove(at: currentCell)
     }
     
     @IBAction func newButton(_ sender: Any) {
         let alertController = UIAlertController(title: "Add new element", message: "Please input your ToDo:", preferredStyle: .alert)
-        
         alertController.addTextField { (textField) in
             textField.placeholder = "ToDo"
         }
-
         let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
             if let newTodoTextField = alertController.textFields?[0] {
-                self.todoList.append(newTodoTextField.text ?? "Error")
+                let toDoToSave = newTodoTextField.text ?? "Error"
+                self.save(toDo: toDoToSave)
                 self.toDoTable.reloadData()
             }
         }
-
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
-
         alertController.addAction(confirmAction)
         alertController.addAction(cancelAction)
-
         present(alertController, animated: true, completion: nil)
-    }
-    @IBAction func deleteButton(_ sender: Any) {
+        dButton.isHidden = false
     }
     
+    @IBAction func deleteButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Delete element", message: "Delete \(todoList[currentCell])?:", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
+            self.toDoTable.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+        self.dButton.isHidden = true
+    }
 }
 
 extension CoreDataViewController: UITableViewDataSource {
@@ -55,11 +98,12 @@ extension CoreDataViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell") as! ToDoTableViewCell
         let currentToDo = todoList[indexPath.row]
-        cell.toDoLabel.text = currentToDo
+        cell.toDoLabel.text = currentToDo.value(forKey: "toDo") as? String
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        currentCell = indexPath.row
+        dButton.isHidden = false
     }
 }
